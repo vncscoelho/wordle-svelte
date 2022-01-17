dd0006d74f32b19fd381d164e57aeeda0f17dd23
Vinicius Coelho <vncs.coelho@gmail.com>
2022-01-17 11:42:04 -0300

    Add word components

---
+++ src/components/WordLetter.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let letter = '';
+</script>
+
+<div class="word-letter">
+	{letter}
+</div>
+
+<style>
+	.word-letter {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex: 0 0 35px;
+		height: 35px;
+		border: 1px dashed grey;
+	}
+</style>
---
+++ src/components/WordRow.svelte
@@ -0,0 +1,16 @@
+<script>
+	import WordLetter from './WordLetter.svelte';
+</script>
+
+<div class="word-row">
+	{#each 'wordle' as letter}
+		<WordLetter />
+	{/each}
+</div>
+
+<style>
+	.word-row {
+		display: flex;
+		justify-content: center;
+	}
+</style>
