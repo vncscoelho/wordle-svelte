d86816600c258091dbe7b979d9438066689b233f
Vinicius Coelho <vncs.coelho@gmail.com>
2022-01-17 11:42:10 -0300

    Add alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			resolve: {
+				alias: {
+					'@': path.resolve('./src')
+				}
+			}
+		}
 	}
 };
 
